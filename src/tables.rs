291527e86742df1faa3ba2996b38b6969411eb61
dVeon-loch <dveonloch@protonmail.com>
2024-12-03 23:45:05 +0200

    Add initial set of tables

--- src/tables.rs
+++ src/tables.rs
@@ -15,3 +15,82 @@ worktable!(
         number_idx: number
     }
 );
+
+worktable!(
+    name: Transaction,
+    columns: {
+        id: u64 primary_key autoincrement,
+        hash: String,
+        status: String,
+        block_number: u64,
+        timestamp_s: u64,
+        from_address: String,
+        to_address: String,
+        internal_transactions: String,
+        value: u64,
+        fee: u64,
+        gas_price: u64
+    }
+    indexes: {
+        hash_idx: hash
+    }
+);
+
+worktable!(
+    name: Address,
+    columns: {
+        id: u64 primary_key autoincrement,
+        hash: u64,
+        address_type: String,
+        type_id: u64,
+        tag: String optional,
+    }
+    indexes: {
+        hash_idx: hash
+    }
+);
+
+worktable!(
+    name: Wallet,
+    columns: {
+        id: u64 primary_key autoincrement,
+        balance: u64,
+        token_holdings: String optional,
+        transactions: String,
+    }
+    indexes: {
+        id_idx: id
+    }
+);
+
+worktable!(
+    name: Contract,
+    columns: {
+        id: u64 primary_key autoincrement,
+        balance: u64,
+        creator: String,
+        tracker: String optional,
+        code: String optional,
+        transactions: String,
+    }
+    indexes: {
+        id_idx: id
+    }
+);
+
+worktable!(
+    name: Token,
+    columns: {
+        id: u64 primary_key autoincrement,
+        token_contract_hash: String,
+        max_supply: u64,
+        price_usd: f64,
+        onchain_cap: u64,
+        circulating_cap: u64,
+        transfers: String,
+        holders: String,
+    }
+    indexes: {
+        token_contract_hash_idx: token_contract_hash
+    }
+);
