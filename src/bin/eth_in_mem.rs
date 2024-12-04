0bfe2c7671631a69fc4fd4a1da7efc3529d43e98
willco-1 <wconyea@crestviewglobal.com>
2024-12-04 18:37:53 -0500

    cleanup

--- src/bin/eth_in_mem.rs
+++ src/bin/eth_in_mem.rs
@@ -1,13 +1,13 @@
 use ethers::{prelude::BlockId, providers::Middleware};
 
+use ethers::types::U256;
 use spice_backend::api::*;
-use std::mem::size_of_val;
 use spice_backend::tables::*;
 use std::error::Error;
-use std::{env, fs::File, io::BufWriter};
-
-use ethers::types::U256;
 use std::sync::atomic::{AtomicU64, Ordering};
+use std::env;
+use tracing::{error, info};
+use tracing_subscriber::FmtSubscriber;
 
 fn u256_to_u64_quads(value: U256) -> [u64; 4] {
     let mut quads = [0u64; 4];
@@ -21,6 +21,11 @@ fn u256_to_u64_quads(value: U256) -> [u64; 4] {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(tracing::Level::ERROR) //set this to info to debug
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
         panic!("Accepts two arguments: start_block end_block");
@@ -28,12 +33,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let start_block = args[1].parse::<u64>()?;
     let end_block = args[2].parse::<u64>()?;
-    println!("Processing blocks from {} to {}", start_block, end_block);
-    let url = "https://mainnet.infura.io/v3/28a2f37d92a942c5951f4a00b431432c".to_string();
+    info!("Processing blocks from {} to {}", start_block, end_block);
+    let url = "https://mainnet.infura.io/v3/28a2f37d92a942c5951f4a00b431432c".to_string(); //This
+    //should use our node but not suitable for local testing
     let api = EthersClient::new(&url, Some("https://eth.llamarpc.com"));
 
-    let mut block_table = BlockWorkTable::default();
-    let mut tx_table = TransactionWorkTable::default();
+    let  block_table = BlockWorkTable::default();
+    let   tx_table = TransactionWorkTable::default();
     let current_id = AtomicU64::new(0);
 
     for block_number in start_block..=end_block {
@@ -41,32 +47,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
             Ok(Some(block)) => {
                 let block_id = current_id.fetch_add(1, Ordering::SeqCst);
 
-                println!("Processing block: {:?}", block.number);
+                info!("Processing block: {:?}", block.number);
                 let block_json = serde_json::to_string(&block)?;
-                println!("Block JSON: {}", block_json);
+                info!("Block JSON: {}", block_json);
 
                 block_table.insert(BlockRow {
                     id: block_id,
                     number: block.number.unwrap_or_default().as_u64(),
                     status: "fetched".to_string(),
                     timestamp_s: block.timestamp.as_u64(),
-                    transactions: block_json,
-                    eth_price_usd_cents: 0,
-                    //TODO:  add cmc here
+                    transactions: block_json, //TODO: Storing block json as string is potentially
+                    //redundant and costly to store
+                    eth_price_usd_cents: 0, //TODO: use cmc lookup here
                 })?;
-                println!("Block inserted with NUMBER: {}", block_number);
+                info!("Block inserted with NUMBER: {}", block_number);
 
-                println!("Block table after insertion: {:?}", block_table);
+
+                info!("Block table after insertion:\n{:#?}", block_table);
 
                 for tx in &block.transactions {
                     let tx_id = current_id.fetch_add(1, Ordering::SeqCst);
-                    let value_quads = u256_to_u64_quads(tx.value);
+                     let value_quads = u256_to_u64_quads(tx.value);
                     let fee_quads = u256_to_u64_quads(
                         U256::from(tx.gas.as_u64()) * tx.gas_price.unwrap_or_default(),
                     );
                     let gas_price_quads = u256_to_u64_quads(tx.gas_price.unwrap_or_default());
 
-                    println!("Transaction details: {:?}", tx);
+                    info!("Transaction details: {:?}", tx);
 
                     tx_table.insert(TransactionRow {
                         id: tx_id,
@@ -78,12 +85,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         to_address: tx
                             .to
                             .map_or_else(|| "".to_string(), |addr| addr.to_string()),
-                        internal_transactions: "".to_string(), //need to fetch these
+                        internal_transactions: "".to_string(),//TODO: this needs to be fetched
                         value_high: value_quads[3],
                         value_mid_high: value_quads[2],
                         value_mid_low: value_quads[1],
                         value_low: value_quads[0],
-                        fee_high: fee_quads[3],
+                        fee_high: fee_quads[3],               // ugh
                         fee_mid_high: fee_quads[2],
                         fee_mid_low: fee_quads[1],
                         fee_low: fee_quads[0],
@@ -92,13 +99,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         gas_price_mid_low: gas_price_quads[1],
                         gas_price_low: gas_price_quads[0],
                     })?;
-                    println!("Transaction inserted with ID: {}", tx_id);
+                    info!("Transaction inserted with ID: {}", tx_id);
 
-                    println!("Transaction table after insertion: {:?}", tx_table);
+
+                    info!("Transaction table after insertion:\n{:#?}", tx_table);
                 }
             }
-            Ok(None) => eprintln!("Block {} not found", block_number),
-            Err(e) => eprintln!("Error fetching block {}: {}", block_number, e),
+            Ok(None) => error!("Block {} not found", block_number),
+            Err(e) => error!("Error fetching block {}: {}", block_number, e),
         }
     }
     Ok(())
